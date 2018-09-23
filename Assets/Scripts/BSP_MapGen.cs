32d84b276ae795146aedeaf600329ddaf22a7d52
rrrobi <rrrrobi@msn.com>
2018-09-24 00:45:25 +0100

    working it out

--- Assets/Scripts/BSP_MapGen.cs
+++ Assets/Scripts/BSP_MapGen.cs
@@ -6,6 +6,7 @@ public class BSP_MapGen : MonoBehaviour {
 
     public struct Segment
     {
+        // Pos = centre
         public float xPos;
         public float yPos;
         public float width;
@@ -14,6 +15,8 @@ public class BSP_MapGen : MonoBehaviour {
 
     const float MAP_WIDTH = 10.0f;
     const float MAP_HEIGHT = 10.0f;
+
+    const int DIVIDE_COUNT = 3;
 
 	// Use this for initialization
 	void Start ()
@@ -24,7 +27,6 @@ public class BSP_MapGen : MonoBehaviour {
 	
     void MapGen()
     {
-        List<List<List<Segment>>> Map = new List<List<List<Segment>>>();
         // Map
         //      ________________
         //      |              |
@@ -51,8 +53,11 @@ public class BSP_MapGen : MonoBehaviour {
         //  List<Segment> A, List<Segment> B - contains all the segments split into 2 lists By the first divide, as shown above
         //  List<Segment> A1, List<Segment> A2, List<Segment> B1, List<Segment> B2 - contains all the segments split into 4 lists By the first 2 divides, as shown above
         //  ... and so on....
-
-        List<Segment> root = new List<Segment>
+        
+        // TODO.. Rethink this
+        List<List<List<Segment>>> Map = new List<List<List<Segment>>>();
+             List<List<Segment>> startLevel = new List<List<Segment>>();
+                  List<Segment> root = new List<Segment>
         {
             new Segment
             {
@@ -62,7 +67,77 @@ public class BSP_MapGen : MonoBehaviour {
                 height = MAP_HEIGHT
             }
         };
+        startLevel.Add(root);
+        Map.Add(startLevel);
 
 
+        #region Step 1 - Build List structure
+        for (int i = 0; i < DIVIDE_COUNT; i++)
+        {
+            // Take last element of List
+            var temp = Map[Map.Count - 1];
+
+            // Decide if cut virtically or horizontaly
+            if (i % 2 == 1)
+
+            // 
+        }
+
+
+        #endregion
+
+        #region Step 2 - Replace individual large segments with list of all small segments that space contains
+
+        #endregion
+    }
+
+
+
+    private List<Segment> SplitSegmentVertical(Segment input)
+    {
+        List<Segment> output = new List<Segment>();
+
+        Segment segment1 = new Segment()
+        {
+            xPos = input.xPos - (input.width / 4),
+            yPos = input.yPos,
+            width = input.width / 2,
+            height = input.height
+        };
+        output.Add(segment1);
+        Segment segment2 = new Segment()
+        {
+            xPos = input.xPos + (input.width / 4),
+            yPos = input.yPos,
+            width = input.width / 2,
+            height = input.height
+        };
+        output.Add(segment2);
+
+        return output;
+    }
+
+    private List<Segment> SplitSegmentHorizontal(Segment input)
+    {
+        List<Segment> output = new List<Segment>();
+
+        Segment segment1 = new Segment()
+        {
+            xPos = input.xPos,
+            yPos = input.yPos - (input.height / 4),
+            width = input.width,
+            height = input.height / 2
+        };
+        output.Add(segment1);
+        Segment segment2 = new Segment()
+        {
+            xPos = input.xPos,
+            yPos = input.yPos + (input.height / 4),
+            width = input.width,
+            height = input.height / 2
+        };
+        output.Add(segment2);
+
+        return output;
     }
 }
