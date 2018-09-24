d07e3c195daba01f52f928801d8153bc4bd108fa
rrrobi <rrrrobi@msn.com>
2018-09-24 21:48:46 +0100

    Stage 1 of BSP complete
    Partitions the designated space by into semi-random spaces.
    Still need to work on the data structure though

--- Assets/Scripts/BSP_MapGen.cs
+++ Assets/Scripts/BSP_MapGen.cs
@@ -13,16 +13,22 @@ public class BSP_MapGen : MonoBehaviour {
         public float height;
     }
 
+    List<List<List<Segment>>> Map = new List<List<List<Segment>>>();
     const float MAP_WIDTH = 10.0f;
     const float MAP_HEIGHT = 10.0f;
+    const float MAX_DIVIDE_RATIO = 0.70f;
+    const float MIN_DIVIDE_RATION = 0.30f;
 
-    const int DIVIDE_COUNT = 3;
+    const int DIVIDE_COUNT = 5;
 
-	// Use this for initialization
-	void Start ()
+    float drawCounter = 0;
+    float counterTimeOut = 2.0f;
+    int drawIndex = 0;
+
+    // Use this for initialization
+    void Start ()
     {
         MapGen();
-		
 	}
 	
     void MapGen()
@@ -55,7 +61,7 @@ public class BSP_MapGen : MonoBehaviour {
         //  ... and so on....
         
         // TODO.. Rethink this
-        List<List<List<Segment>>> Map = new List<List<List<Segment>>>();
+//        List<List<List<Segment>>> Map = new List<List<List<Segment>>>();
              List<List<Segment>> startLevel = new List<List<Segment>>();
                   List<Segment> root = new List<Segment>
         {
@@ -85,7 +91,7 @@ public class BSP_MapGen : MonoBehaviour {
                 {
                     foreach (var segment in segmentList)
                     {
-                        newLevel.Add(SplitSegmentHorizontal(segment));
+                        newLevel.AddRange(SplitSegmentHorizontal(segment));
                     }
                     
                 }
@@ -96,7 +102,7 @@ public class BSP_MapGen : MonoBehaviour {
                 {
                     foreach (var segment in segmentList)
                     {
-                        newLevel.Add(SplitSegmentVertical(segment));
+                        newLevel.AddRange(SplitSegmentVertical(segment));
                     }
                     
                 }
@@ -116,51 +122,117 @@ public class BSP_MapGen : MonoBehaviour {
 
 
 
-    private List<Segment> SplitSegmentVertical(Segment input)
+    private List<List<Segment>> SplitSegmentVertical(Segment input)
     {
-        List<Segment> output = new List<Segment>();
+        List<List<Segment>> output = new List<List<Segment>>();
+
+        float seg1Ratio = RandomRatio();
+        float seg2Ratio = 1.0f - seg1Ratio;
 
         Segment segment1 = new Segment()
         {
-            xPos = input.xPos - (input.width / 4),
+            // center of new Left segment = 
+            // original center  
+            // -Half Width  
+            // +Half Ratio of original Full Width
+            xPos = input.xPos - (input.width / 2) + (input.width * (seg1Ratio / 2)),
             yPos = input.yPos,
-            width = input.width / 2,
+            width = input.width * seg1Ratio,
             height = input.height
         };
-        output.Add(segment1);
+        output.Add(new List<Segment>() { segment1 });
         Segment segment2 = new Segment()
         {
-            xPos = input.xPos + (input.width / 4),
+            // center of new Right segment = 
+            // original center  
+            // +Half Width  
+            // -Half Ratio of original Full Width
+            xPos = input.xPos + (input.width / 2) - (input.width * (seg2Ratio / 2)),
             yPos = input.yPos,
-            width = input.width / 2,
+            width = input.width * seg2Ratio,
             height = input.height
         };
-        output.Add(segment2);
+        output.Add(new List<Segment>() { segment2 });
 
         return output;
     }
 
-    private List<Segment> SplitSegmentHorizontal(Segment input)
+    private List<List<Segment>> SplitSegmentHorizontal(Segment input)
     {
-        List<Segment> output = new List<Segment>();
+        List<List<Segment>> output = new List<List<Segment>>();
+
+        float seg1Ratio = RandomRatio();
+        float seg2Ratio = 1.0f - seg1Ratio;
 
         Segment segment1 = new Segment()
         {
+            // center of new Top segment = 
+            // original center  
+            // +Half Hieght  
+            // -Half Ratio of original Full Hieght
             xPos = input.xPos,
-            yPos = input.yPos - (input.height / 4),
+            yPos = input.yPos + (input.height / 2) - (input.height * (seg1Ratio / 2)),
             width = input.width,
-            height = input.height / 2
+            height = input.height * seg1Ratio
         };
-        output.Add(segment1);
+        output.Add(new List<Segment>() { segment1 });
         Segment segment2 = new Segment()
         {
+            // center of new Bottom segment = 
+            // original center  
+            // -Half Hieght  
+            // +Half Ratio of original Full Hieght
             xPos = input.xPos,
-            yPos = input.yPos + (input.height / 4),
+            yPos = input.yPos - (input.height / 2) + (input.height * (seg2Ratio / 2)),
             width = input.width,
-            height = input.height / 2
+            height = input.height * seg2Ratio
         };
-        output.Add(segment2);
+        output.Add(new List<Segment>() { segment2 });
 
         return output;
+    }
+
+    private float RandomRatio()
+    {
+        return Random.Range(MIN_DIVIDE_RATION, MAX_DIVIDE_RATIO);
+    }
+
+    void Update()
+    {
+        
+        drawCounter += Time.deltaTime;
+        if (drawCounter >= counterTimeOut)
+        {
+            drawIndex++;
+            drawCounter = 0;
+            if (drawIndex >= Map.Count)
+            {
+                drawIndex = 0;
+                MapGen();
+            }
+        }
+
+
+        foreach (var segList in Map[drawIndex])
+        {
+            foreach (var segment in segList)
+            {
+                DrawSegment(segment);
+
+            }
+        }
+    }
+
+    private void DrawSegment(Segment input)
+    {
+        Vector3 topLeft = new Vector3(input.xPos - (input.width/2), input.yPos + (input.height/2), 0.0f);
+        Vector3 topRight = new Vector3(input.xPos + (input.width / 2), input.yPos + (input.height / 2), 0.0f);
+        Vector3 bottomLeft = new Vector3(input.xPos - (input.width / 2), input.yPos - (input.height / 2), 0.0f);
+        Vector3 bottomRight = new Vector3(input.xPos + (input.width / 2), input.yPos - (input.height / 2), 0.0f);
+
+        Debug.DrawLine(topLeft, topRight);
+        Debug.DrawLine(topRight, bottomRight);
+        Debug.DrawLine(bottomRight, bottomLeft);
+        Debug.DrawLine(bottomLeft, topLeft);
     }
 }
